4b164d7beb5802cdfbc00cd41e7f0c888e9380cf
vanshvirmani02 <122612877+vanshvirmani02@users.noreply.github.com>
2024-05-29 12:11:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize: {
+        '10xl': ['10rem', { lineHeight: '2' }]
+      }
+    },
+  },
+  plugins: [],
+}
+
